9fc6633e91cc428ae294983f0617178a239075bb
Belal Salah <belal.salah259@gmail.com>
2026-01-16 22:45:01 +0200

    adding autorization

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,16 @@
 <?php
 
-use App\Http\Controllers\Api\v1\PostController ;
+use App\Http\Controllers\Api\v1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('posts', PostController::class);
+    });
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// Route::apiResource('posts', PostController::class);    
-  
-
-Route::prefix('v1')->group(function () {
-    Route::apiResource('posts', PostController::class);
-}); 
+require __DIR__.'/auth.php';
