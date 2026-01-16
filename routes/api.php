5b733534974dffb819b61f6af12d05b5387e7329
Belal Salah <belal.salah259@gmail.com>
2026-01-16 20:22:55 +0200

    create post crud operation with resources

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\v1\PostController ;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Route::apiResource('posts', PostController::class);    
+  
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('posts', PostController::class);
+}); 
